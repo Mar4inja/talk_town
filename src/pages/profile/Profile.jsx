e9f5705667bd6f59c692f4b0eebacb007c3c6c78
Mar4inja <mar4inja19@gmail.com>
2024-12-25 00:00:23 +0100

    update

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,8 +1,11 @@
 import styles from "./profile.module.css";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../images/logo/logotips-removebg-preview.png";
 
 const Profile = () => {
+  const [messageCount, setMessageCount] = useState(5);
+  const [profilePhoto, setProfilePhoto] = useState(null);
+
   useEffect(() => {
     document.body.classList.add(styles.blueBackground);
     return () => {
@@ -12,20 +15,28 @@ const Profile = () => {
 
   return (
     <div className={styles.appContainer}>
-      {/* Profila Header konteiners */}
+      {/* Profile Header Container */}
       <div className={styles.profileContainer}>
         <div className={styles.profileHeader}>
-          <div className={styles.greyLine}></div>
           <img src={logo} alt="LOGO" className={styles.headerLogo} />
           <input
             type="text"
             className={styles.profileSearchBar}
             placeholder="Search..."
           />
+          <div className={styles.headerButtons}>
+            <a href="#" className={styles.link}>Friends</a>
+            <a href="#" className={styles.link}>Messages</a>
+            <div className={styles.messageCountContainer}>{messageCount}</div>
+          </div>
+          <div className={styles.profileCircle}></div>
         </div>
-        <div className={styles.headerButtons}>
-          <a href="#" className={styles.link}>Friends</a>
-          <a href="#" className={styles.link}>Messages</a>
+        {/* Profile Content Container */}
+        <div className={styles.profileContentContainer}>
+          <div className={styles.profilePhotoContainer}>
+            {/* Profile Image inside profilePhotoContainer */}
+            <div className={styles.profileImage}></div>
+          </div>
         </div>
       </div>
     </div>
